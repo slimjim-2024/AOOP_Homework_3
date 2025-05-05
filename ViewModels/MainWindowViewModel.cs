be90c46830a7479ede8f66a8e99e3b29bce2b040
Vlad Slimovschi <vlsli24@student.sdu.dk>
2025-05-05 10:15:10 +0200

    Commit cause yes

--- ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AOOP_HOMEWORK_3.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace AOOP_HOMEWORK_3.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
